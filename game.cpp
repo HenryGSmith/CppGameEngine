030287c0fb128e44eae6ae331010591b90d7c9e4
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-07-15 17:35:37 +0100

    building control system for pong

--- game.cpp
+++ game.cpp
@@ -2,18 +2,36 @@
 #define pressed(b) (input->buttons[b].is_down && input->buttons[b].changed)
 #define released(b) !(input->buttons[b].is_down && input->buttons[b].changed)
 
-float player_pos_x = 0.f;
-float player_pos_y = 0.f;
+float player_1_p, player_1_dp, player_2_p, player_2_dp;
+
+u32 white = 0xf7f4f3;
+u32 black = 0x000011;
 
 internal void 
 simulate_game(Input *input, float dt) {
-	clear_screen(0x000011);
+	clear_screen(black);
 	
-	float speed = 50;
-	if (is_down(BUTTON_UP))    player_pos_y += speed * dt;
-	if (is_down(BUTTON_LEFT))  player_pos_x -= speed * dt;
-	if (is_down(BUTTON_RIGHT)) player_pos_x += speed * dt;
-	if (is_down(BUTTON_DOWN))  player_pos_y -= speed * dt;
-		
-	draw_rect(player_pos_x, player_pos_y, 20, 20, 0xffffff);
+	// player 1
+	float player_1_ddp = 0.f;
+	if (is_down(BUTTON_W))    player_1_ddp += 2000;
+	if (is_down(BUTTON_S))  player_1_ddp -= 2000;
+
+	player_1_ddp -= player_1_dp * 10.f;
+
+	player_1_p = player_1_p + player_1_dp * dt + player_1_ddp * dt * dt * .5f;
+	player_1_dp = player_1_dp + player_1_ddp * dt;
+
+	// player 2
+	float player_2_ddp = 0.f;
+	if (is_down(BUTTON_UP))    player_2_ddp += 2000;
+	if (is_down(BUTTON_DOWN))  player_2_ddp -= 2000;
+
+	player_2_ddp -= player_2_dp * 10.f;
+
+	player_2_p = player_2_p + player_2_dp * dt + player_2_ddp * dt * dt * .5f;
+	player_2_dp = player_2_dp + player_2_ddp * dt;
+	
+	// render
+	draw_rect(-80, player_1_p, 2, 12, white);
+	draw_rect(80, player_2_p, 2, 12, white);
 }
--- win32_platform.cpp
+++ win32_platform.cpp
@@ -95,15 +95,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 #define process_button(b, vk)\
 case vk:{\
+input.buttons[b].changed = is_down != input.buttons[b].is_down;\
 input.buttons[b].is_down = is_down;\
-input.buttons[b].changed = true;\
 }break;\
 
 					switch (vk_code) {
 						process_button(BUTTON_UP, VK_UP);
 						process_button(BUTTON_DOWN, VK_DOWN);
-						process_button(BUTTON_LEFT, VK_LEFT);
-						process_button(BUTTON_RIGHT, VK_RIGHT);
+						process_button(BUTTON_W, 'W');
+						process_button(BUTTON_S, 'S');
 					}
 				}break;
 				default: {
